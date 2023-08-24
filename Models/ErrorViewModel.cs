f54638490d7320c44643c407c3785e558a27a9aa
Rhonda Guillory <guilloryrt.ps@gmail.com>
2023-08-23 21:59:12 -0500

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPNET2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
